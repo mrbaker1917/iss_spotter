410a35f8938a6bcbb459a43bfd1bb47b46b560e1
Mark R Baker <mrbaker1917@gmail.com>
2020-08-28 07:23:49 -0700

    Changed seconds to seconds and minutes

--- index2.js
+++ index2.js
@@ -4,8 +4,9 @@ const printPassTimes = function(flyovers) {
   for (let time of flyovers) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(time.risetime);
-    const duration = time.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    const durationMins = Math.round(time.duration / 60);
+    const durationSecs = time.duration % 60;
+    console.log(`Next pass at ${datetime} for ${durationMins} minutes and ${durationSecs} seconds!`);
   }
 };
 
